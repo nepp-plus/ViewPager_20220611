a6b1a48bfa00555e61c57b3eedf7e0843e08c41d
조경진 <cho881020@gmail.com>
2022-06-11 11:08:34 +0900

    물려받은 추상 함수 (필수 구현 함수) 2개 작성

--- app/src/main/java/com/neppplus/viewpager_20220611/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/neppplus/viewpager_20220611/adapters/MainViewPagerAdapter.kt
@@ -1,7 +1,27 @@
 package com.neppplus.viewpager_20220611.adapters
 
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.neppplus.viewpager_20220611.fragments.AddressFragment
+import com.neppplus.viewpager_20220611.fragments.HelloFragment
+import com.neppplus.viewpager_20220611.fragments.NameFragment
 
 class MainViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getCount(): Int {
+
+        return 3
+    }
+
+    override fun getItem(position: Int): Fragment {
+        if ( position == 0 ) {
+            return HelloFragment()
+        }
+        else if (position == 1) {
+            return NameFragment()
+        }
+        else {
+            return AddressFragment()
+        }
+    }
 }
